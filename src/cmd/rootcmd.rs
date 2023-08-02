99f93e39575334403b49f1c9ffd810e15821ee81
jiashiwen <jiashiwen@ZBMac-C02F34Q9MD6M.local>
2023-08-02 14:05:26 +0800

    new log

--- src/cmd/rootcmd.rs
+++ src/cmd/rootcmd.rs
@@ -201,7 +201,7 @@ fn cmd_match(matches: &ArgMatches) {
                 match r {
                     Ok(_) => {}
                     Err(e) => {
-                        log::error!("{:?}{}", vec_lp, e);
+                        log::error!("{:?}{}", json_str, e);
                     }
                 }
             }
